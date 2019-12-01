5350f090c79ecb6bf806c9ad81a1c0c8dc915140
kokoforworldpeace <kokoforworldpeace@gmail.com>
2019-12-01 13:11:31 -0500

    new Stmts

--- statements/AddStmt.cc
+++ statements/AddStmt.cc
@@ -1,10 +1,19 @@
-#include "../headerFiles/Statement.h"
-#include <mainwindow.h>
+#include "../headerFiles/AddStmt.h"
 using namespace std;
 
-class ReadStmt : public Statement {
-  public:
-    ReadStmt();
-    void compile(QString *instr){}
-    void run(){};
-};
+AddStmt::AddStmt(QString labelname, QString inst, QString op1, QString op2)
+  :Statement(labelname, inst, op1, op2){}
+
+void AddStmt::compile(){
+  if(label->getName() != nullptr){
+    statementObj["Label"] = label->getName();
+  }
+
+  statementObj["Instruction"] = instruction;
+
+  statementObj["Operand1"] = this->getFirstOperand();
+
+  statementObj["Operand2"] = this->getSecondOperand();
+}
+
+void AddStmt::run(){}
--- statements/MovStmt.cc
+++ statements/MovStmt.cc
@@ -1,9 +1,19 @@
-#include "../headerFiles/Statement.h"
+#include "../headerFiles/MoveStmt.h"
 using namespace std;
 
-class ReadStmt : public Statement {
-  public:
-    ReadStmt(){}
-    void compile(string *instr){}
-    void run(){};
-};
+MoveStmt::MoveStmt(QString labelname, QString inst, QString op1, QString op2)
+  :Statement(labelname, inst, op1, op2){}
+
+void MoveStmt::compile(){
+  if(label->getName() != nullptr){
+    statementObj["Label"] = label->getName();
+  }
+
+  statementObj["Instruction"] = instruction;
+
+  statementObj["Operand1"] = this->getFirstOperand();
+
+  statementObj["Operand2"] = this->getSecondOperand();
+}
+
+void MoveStmt::run(){}
